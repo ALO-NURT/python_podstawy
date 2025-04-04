9117654a3d045af2b7f9eacc4b99278e4821e03f
wojtek_maniak <mani-wo@nurt.net.pl>
2025-04-04 08:09:09 +0200

    skibidi

--- src/python_podstawy.py
+++ src/python_podstawy.py
@@ -3,16 +3,18 @@
 # To jest komentarz - nie jest wykonywany przez interpreter
 
 # zmienne
-zmienna = 5 # to jest zmienna typu int
-ciag_znakow = "To jest ciag znakow" # to jest zmienna typu str
-zmienna_float = 5.5 # to jest zmienna typu float
-zmienna_bool = True # to jest zmienna typu bool
-liczba = zmienna # do zmiennej liczba przypisujemy wartość zmiennej zmienna
-zmienna = "Pięć" # w Pythonie zmienne mogą zmieniać typ - typowanie jest dynamiczne - ale nie jest to zalecane (kod jest mniej czytelny)
+zmienna = 5  # to jest zmienna typu int
+ciag_znakow = "To jest ciag znakow"  # to jest zmienna typu str
+zmienna_float = 5.5  # to jest zmienna typu float
+zmienna_bool = True  # to jest zmienna typu bool
+liczba = zmienna  # do zmiennej liczba przypisujemy wartość zmiennej zmienna
+zmienna = "Pięć"  # w Pythonie zmienne mogą zmieniać typ - typowanie jest dynamiczne - ale nie jest to zalecane (kod
+# jest mniej czytelny)
+print("Wojciech Maniak")
 
 # podstawowe funkcje
-print("Hello World!") # to jest funkcja, która wypisuje tekst podany jako argument
-print(zmienna) # wypisuje zmienną na ekranie
+print("Hello World!")  # to jest funkcja, która wypisuje tekst podany jako argument
+print(zmienna)  # wypisuje zmienną na ekranie
 print(liczba)
 
 # operatory
