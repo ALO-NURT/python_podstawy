a93291eb7adeaffdc71932b835fee491eea385b3
Wiktoria Olaczek <Olac-Wi@nurt.net.pl>
2025-04-16 09:00:10 +0200

    praca na lekcji 14.04.2025

--- src/python_podstawy.py
+++ src/python_podstawy.py
@@ -18,3 +18,42 @@ print("Wiktoria Olaczek")
 # operatory
 
 print ("zadanie domowe")
+
+
+##lekcja 16.04.2025##
+#operatory
+a = 6
+print(1 < a)
+print(5==5)
+print(8 <= 3)
+
+liczba = 10
+print(liczba + 1)
+print(liczba * 3)
+print(liczba / 3)
+print(liczba // 3) #dzielenie całkowite
+print(liczba -3)
+print(liczba % 3) #reszta z dzielenia
+
+#typy złożone
+lista = [1,2,4,8,16,32]
+krotka = (1,2,4,8,16,32)
+print(lista)
+print(lista[0])
+lista[0]=5
+lista.append(64)
+print(lista)
+
+lista[0]=1
+print(lista)
+#insturkcja warunkowa
+if lista == 1:
+    print("pierwszy element listy to jeden")
+    if lista[1] == 2:
+        print("drugi element to dwa")
+    elif lista[2] == 4:
+        print("trzeci element to cztery")
+    else:
+        print("drugi element nie jest dwójką")
+
+
