38024810e939b6c1e0b86ba282616847b6902fa5
urszula dunajska <duna-ur@nurt.net.pl>
2025-04-16 08:58:36 +0200

    Writing and reading by email lowercase Początek nauki języka Python lekcja 16.04

--- src/python_podstawy.py
+++ src/python_podstawy.py
@@ -16,3 +16,28 @@ print(zmienna) # wypisuje zmienną na ekranie
 print(liczba)
 
 # operatory
+a = 6  a:6
+print(1 < a)
+print(5 == 5)
+print(8 <= 5)
+
+liczba = 10
+print(liczba + 1)
+print(liczba + 3)
+print(liczba/3)
+print(liczba//3) #dzielenie całkowite
+print(liczba - 3)
+print(liczba % 3) #reszta z dzielenia
+
+#typy złożone
+lista = (1, 3, 5, 8, 23)
+print(lista)
+print(lista(0))
+
+if  lista(0) == 1
+    print("pierwszy element listy to jeden")
+
+
+
+
+
