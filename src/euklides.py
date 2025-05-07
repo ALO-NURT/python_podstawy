5e9cad0afca1881a662f9d204112b79d8527eba5
Zuzann Szydło <szyd-zu@nurt.net.pl>
2025-05-07 08:40:28 +0200

    lekcje kwietniowe

---
+++ src/euklides.py
@@ -0,0 +1,14 @@
+# lekcja 30.04.2025
+
+silnia = 1
+parametr = 5
+i = 1
+
+while i <= parametr:
+    silnia *= i
+    i += 1
+
+    print(silnia)
+
+#NWD - największy wspólny dzielnik
+#NWW - najmniejsza wspólna wielokrotność
