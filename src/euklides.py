15c10b8230aff2ca4c3e0dc0a9ff882b4642c4c7
helena borkowska <bork-he@nurt.net.pl>
2025-05-07 10:18:59 +0200

    07.05 praca na lekcji

--- src/euklides.py
+++ src/euklides.py
@@ -7,3 +7,8 @@ def nwd (a, b):
         return nwd(b, a % b)
 
 print(f"Największym wspólnym dzielnikiem jest {nwd(10,15)}")
+
+def nww(a, b):
+    return (a * b) // nwd(a, b)
+
+print(f"Najmniejsza wspólna wielokrotność 10 i 15 to {nww( a=10, b=15)}")
