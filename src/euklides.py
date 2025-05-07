eb97f33fb4b3397aa4421942f029ddfb5739614a
Mary-Ga <mary-ga@nurt.net.pl>
2025-05-07 09:02:41 +0200

    07.05.2025

---
+++ src/euklides.py
@@ -0,0 +1,10 @@
+def nwd (a,b) :
+    if b>a :
+        return nwd (b,a)
+    elif a == b or b == 0:
+        return a
+    else:
+        return nwd (b, a % b)
+
+print(f"Największym wspólnym dzielnikiem jest {nwd(a=10, b=15)}")
+
