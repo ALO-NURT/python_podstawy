efa0e3ed21a4650ff867473467f01e7d4efd4717
Mary-Ga <mary-ga@nurt.net.pl>
2025-05-07 09:37:07 +0200

    zadanie domowe z 7 maja 2025 r.

--- src/euklides.py
+++ src/euklides.py
@@ -8,14 +8,8 @@ def nwd (a,b) :
 
 print(f"Największym wspólnym dzielnikiem jest {nwd(a=10, b=15)}")
 
-def nwd(a, b):
-    if b > a:
-        return nwd(b, a)
-    elif a == b or b == 0:
-        return a
-    else:
-        return nwd(b, a % b)
-        def nww(a, b):
-            return (a * b) // nwd(a, b)
+
+def nww(a, b):
+    return (a * b) // nwd(a, b)
 
 print(f"Najmniejsza wspólna wielokrotność 20 i 25 to {nww( a=20, b=25)}")
