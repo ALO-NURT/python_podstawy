c35c7199cccbaa826110f558610c3ad014e8e732
Weronika Mrugala <mrug-we@nurt.net.pl>
2025-05-07 10:32:25 +0200

    praca na lekcji

--- src/euklides.py
+++ src/euklides.py
@@ -7,3 +7,8 @@ def nwd (a, b):
         return nwd(b, a % b)
 
 print(f"najwiekszym wspólnym dzielnikiem jest {nwd(a=10, b=15)}")
+
+
+def nww(a,b)
+    return (a * b )// nwd(a,b)
+print(f"najmniejsza wspólna wielokrotność 5 i 8 to {nww(a=5,b=8)}")
