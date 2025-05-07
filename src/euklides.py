73ddfa38b97cdf656df7c696490ecdd10c7af787
Róża Figiel <figi-ro@nurt.net.pl>
2025-05-07 09:23:55 +0200

    Zadanie domowe, mam nadzieję, że dobrze, bo robiłam z Zuzanną S

--- src/euklides.py
+++ src/euklides.py
@@ -7,3 +7,8 @@ def nwd(a,b):
         return nwd(b, a % b)
 
 print(f"Największym wspólnym dzielnikiem jest {nwd(a=30, b=32)}")
+
+def nww(a, b):
+    return (a * b) // nwd(a, b)
+
+print(f"Najmniejsza wspólna wielokrotność to {nww(30, 32)}")
