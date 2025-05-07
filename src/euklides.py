b3130aab7c5bd5d21455126c1d028a2e34f050c2
Oliwia Szymańska <szym-ol@nurt.net.pl>
2025-05-07 10:04:05 +0200

    zadanie domowe z lekcji o NWW NWD

--- src/euklides.py
+++ src/euklides.py
@@ -7,3 +7,23 @@ def nwd (a, b):
         return nwd (b, a % b )
 
 print(f"największy wspólnym dzielnikiem jest {nwd(10,15)}")
+
+
+def nwd(a, b):
+    if b > a:
+        return nwd(b, a)
+    elif a == b or b == 0:
+        return a
+    else:
+        return nwd(b, a % b)
+
+
+def nww(a, b):
+    return abs(a * b) // nwd(a, b)
+
+
+a = 15
+b = 20
+
+print(f"Największy wspólny dzielnik {a} i {b} to {nwd(a, b)}")
+print(f"Najmniejsza wspólna wielokrotność {a} i {b} to {nww(a, b)}")
