491a337562a3586b03b32e654b125f3d3d396c2c
helena borkowska <bork-he@nurt.net.pl>
2025-05-07 09:00:53 +0200

    07.05 praca na lekcji

---
+++ src/euklides.py
@@ -0,0 +1,9 @@
+def nwd (a, b):
+    if b>a :
+        return nwd(b, a)
+    elif a == b or b == 0:
+        return a
+    else:
+        return nwd(b, a % b)
+
+print(f"Największym wspólnym dzielnikiem jest {nwd(10,15)}")
