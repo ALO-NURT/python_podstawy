2bc76051ff904e5a5f662b6da8e15bd3aaaeb70d
Zuzann Szydło <Szyd-Zu@nurt.net.pl>
2025-05-07 09:15:22 +0200

    lekcje

--- src/euklides.py
+++ src/euklides.py
@@ -1,6 +1,6 @@
 # lekcja 7.05.2025
 # NWD
-# NWW
+
 def nwd(a ,b):
     if b > a :
         return nwd(b, a)
@@ -10,3 +10,17 @@ def nwd(a ,b):
         return nwd(b, a % b)
 
 print(f"Największym wspólnym mianownikiem jest {nwd( a=10, b=15)}")
+
+#NWW
+
+def nwd(a, b):
+    if b > a:
+        return nwd(b, a)
+    elif a == b or b == 0:
+        return a
+    else:
+        return nwd(b, a % b)
+def nww(a, b):
+    return (a * b) // nwd(a, b)
+
+print(f"Najmniejsza wspólna wielokrotność 10 i 15 to {nww( a=10, b=15)}")
