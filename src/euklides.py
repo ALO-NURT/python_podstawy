849ca521906bbf5c148556b1ba55cb9669652eff
Maria Lekszycka <leks-ma@nurt.net.pl>
2025-05-07 09:01:01 +0200

    Lekcja 07.05.25

---
+++ src/euklides.py
@@ -0,0 +1,10 @@
+def nwd(a,b):
+    if b>a :
+        return nwd(b,a)
+    elif a==b or b==0:
+        return a
+    else:
+        return nwd(b, a % b)
+
+print(f"Największym wspólnym dzielnikiem jest {nwd(30,32)}")
+
