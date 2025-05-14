a4babd2014757c0b59534b5072f18ec711769e68
antonina zawierta <zawi-an@nurt.net.pl>
2025-05-14 08:44:42 +0200

    lekcja 14.05.2025 ciąg fibonacciego

---
+++ src/fibonacci.py
@@ -0,0 +1,9 @@
+def ciag_fibonacciego(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return ciag_fibonacciego(n - 1) + ciag_fibonacciego(n - 2)
+
+print("6. liczba ciągu Fibonacciego to:", ciag_fibonacciego(6))
