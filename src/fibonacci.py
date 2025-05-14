0017af41d4ae5cca2b290067ed503353c99034ca
Weronika Mrugala <mrug-we@nurt.net.pl>
2025-05-14 09:03:31 +0200

    praca na lekcji

---
+++ src/fibonacci.py
@@ -0,0 +1,13 @@
+
+def fibonacci(n):
+   elif n==1 or n==2:
+       return 1
+   else:
+       return fibonacci(n-1)+fibonacci(n-2)
+
+def fibonacci(n):
+    return fibonacci(n) ** 2
+
+
+print(fibonacci(6))
+
