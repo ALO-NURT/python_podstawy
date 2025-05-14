ee06b48559e019abe8065bdf642fd35b2425c6c3
Jagna Biedka <bied-ja@nurt.net.pl>
2025-05-14 08:33:11 +0200

    lekcja 30.04.2205

---
+++ src/euklides.py
@@ -0,0 +1,13 @@
+def nwd (a, b):
+    if b>a :
+        return nwd(b, a)
+    elif a == b or b == 0:
+        return a
+    else:
+        return nwd(b, a % b)
+print(f"Największym wspólnym dzielnikiem jest {nwd(10,15)}")
+
+def nww(a, b):
+    return (a * b) // nwd(a, b)
+
+print(f"Najmniejsza wspólna wielokrotność 10 i 15 to {nww( a=10, b=15)}")
