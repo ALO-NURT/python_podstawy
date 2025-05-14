857e6527760996287a49477a6d11e8ea9a407b51
Oliwia Szymańska <szym-ol@nurt.net.pl>
2025-05-14 08:51:59 +0200

    lekcja 14.05.2025 ciąg fibonnaciego

---
+++ src/fibonaci.py
@@ -0,0 +1,14 @@
+def ciag_fibonacciego(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return ciag_fibonacciego(n - 1) + ciag_fibonacciego(n - 2)
+
+
+print("6-ta liczba ciągu Fibonacciego to:", ciag_fibonacciego(6))
+
+
+
+
