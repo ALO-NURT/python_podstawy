e2224d7037bfeed119e090f627782427e3734a0c
JanMisTom <Rych-Ja@nurt.net.pl>
2025-05-16 19:44:48 +0200

    lekcja 16.05.2025 euklides zadanie domowe

--- src/euklides.py
+++ src/euklides.py
@@ -1,3 +1,18 @@
-def nwd(a, b)
+def nwd(a, b):
+    if b > a:
+        return nwd(b, a)
+    elif a == b or b == 0:
+        return a
+    return nwd(b, a % b)
 
-def nww(a, b)
+
+def nww(a, b):
+    return (a * b) // nwd(a, b)
+
+
+
+a = int(input("Podaj pierwszą liczbę "))
+b = int(input("Podaj drugą liczbę "))
+
+print(f"Największy wspólny dzielnik: {nwd(a, b)}")
+print(f"Najmniejsza wspólna wielokrotność: {nww(a, b)}")
