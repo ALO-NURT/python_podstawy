7e2063630d15b7884730b578503e208ef788f6a2
urszula dunajska <duna-ur@nurt.net.pl>
2025-05-21 09:00:29 +0200

    Writing and reading by email lowercase Lekcja 21.05.25 - definiowanie liczb pierwszych

---
+++ src/faktoryzacja.py
@@ -0,0 +1,11 @@
+from cmath import sqrt
+
+n =
+def liczba_pierwsza (n):
+    if n<2:
+        return False
+    for i in range (2, sqrt(n)):
+        if n%i == 0:
+            return False
+    return True
+print(liczba_pierwsza(n))
