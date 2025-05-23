ab7978bb7ba5539ea43347b13b7f85f6cc4f0954
Jan Szychowski <szyc-ja@nurt.net.pl>
2025-05-23 08:02:32 +0200

    Zadanie domowe na 16.05.2025 A.D. - poprawka

--- src/Euklides.py
+++ src/Euklides.py
@@ -1,12 +1,13 @@
 def nwd(a, b):
-    """Zwraca największy wspólny dzielnik (NWD) dwóch liczb a i b, używając algorytmu Euklidesa."""
     while b != 0:
         a, b = b, a % b
-    return abs(a)
+    return a
 
+print(nwd(48, 18))
+
+import math
 
 def nww(a, b):
-    """Zwraca najmniejszą wspólną wielokrotność (NWW) dwóch liczb a i b."""
-    if a == 0 or b == 0:
-        return 0
-    return abs(a * b) // nwd(a, b)
+    return abs(a * b) // math.gcd(a, b)
+
+print(nww(12, 18))
