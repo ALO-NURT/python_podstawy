c2e6882f3d4171ad4a5fe8e8cb2b4bc8affc78de
Mary-Ga <mary-ga@nurt.net.pl>
2025-05-28 08:59:35 +0200

    praca na lekcji faktoryzacja 28.05.2025

--- src/faktoryzacja.py
+++ src/faktoryzacja.py
@@ -1,11 +1,10 @@
 def liczba_pierwsza (n):
     if n<2:
         return False
-    for i in range(2, (int sqrt(n))):
+    for i in range(2,int (sqrt(n))+1):
         if n % i == 0:
             return False
     return True
 
 for liczba in range (0,1000):
     print(f"Liczba {liczba} jest pierwsza? -> {liczba_pierwsza(liczba)}")
-
