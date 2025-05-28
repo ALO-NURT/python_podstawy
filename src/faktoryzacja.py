dad6e3fb1c048dff73bf5035a3d3ae86818b01de
urszula dunajska <duna-ur@nurt.net.pl>
2025-05-28 09:00:09 +0200

    Writing and reading by email lowercase Lekcja 21.05.25 - definiowanie liczb pierwszych int sqrt nie działa :( invalid syntax :(

--- src/faktoryzacja.py
+++ src/faktoryzacja.py
@@ -1,11 +1,24 @@
-from cmath import sqrt
+from math import sqrt
 
-n =
+n = 25
 def liczba_pierwsza (n):
     if n<2:
         return False
-    for i in range (2, int(sqrt(n))):
+    for i in range (2, int(sqrt(n)) + 1):
         if n%i == 0:
             return False
     return True
 print(liczba_pierwsza(n))
+
+
+wynik = 0
+for liczba in range(10):
+    if liczba % 2 == 0:
+        wynik = wynik + liczba
+    else:
+        wynik = wynik - liczba
+print(wynik)
+
+# sito erastotenesa ---> zawsze zaczyna od dwójki i dodsje kolejne liczby
+
+[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
