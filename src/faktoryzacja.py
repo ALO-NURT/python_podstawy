11d9fcc1e600b43a72ab5ffb5040bbb970a332c7
Oliwia Szymańska <szym-ol@nurt.net.pl>
2025-05-28 08:59:31 +0200

    lekcja 28.05.2025

--- src/faktoryzacja.py
+++ src/faktoryzacja.py
@@ -1,7 +1,7 @@
 
 from math import sqrt
 def liczba_pierwsza (n):
-    for i in range (2 , int(sqrt(n))):
+    for i in range (2 , int(sqrt(n))+1):
         if n % i == 0:
             return False
     return True
