43cf1f0e535e65d09fa0ffac323cbb88c7402435
Mary-Ga <mary-ga@nurt.net.pl>
2025-06-11 09:04:46 +0200

    praca na lekcji faktoryzacja 21.05.2025

--- src/faktoryzacja.py
+++ src/faktoryzacja.py
@@ -8,3 +8,18 @@ def liczba_pierwsza (n):
 
 for liczba in range (0,1000):
     print(f"Liczba {liczba} jest pierwsza? -> {liczba_pierwsza(liczba)}")
+
+
+def sito_erastotenesa (n):
+    if n<2:
+        return[ ]
+    lista= [True]*n
+    print(lista)
+    sito_erastotenesa(2)
+while indeks i range (2,int (n**0.5) + 1):
+    if lista [indeks]:
+        for i in (2* indeks, n+1, indeks):
+            lista [i]= False
+    return (lista):
+    lista[i]=False
+    wynik= [ ]
