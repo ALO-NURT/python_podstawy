c3c5a5c74c11647f4012aacb54b461fb6c77d29c
Weronika Mrugala <mrug-we@nurt.net.pl>
2025-06-11 09:03:23 +0200

    praca na lekcji

--- faktoryzacja.py
+++ faktoryzacja.py
@@ -5,3 +5,22 @@ def liczba_pierwsza(n):
           return False
     return True
 print(liczba_pierwsza(15))
+
+  def sito-erastotelesa(n):
+   lista = [True]*n
+   print(lista)
+ sito erystotelesa(2)
+    if n<2
+        return[]
+    lista=[true]*(n+1)
+    lista[0]=False
+    indeks=2
+    for indeks in range(2,int(n**0.5)+1)
+        if lista[indeks]:
+            for i in range(2*indeks.n+1.indeks)
+                lista[i]=false
+            return(lista)
+   wynik=[]
+   for i in range(n+1):
+      if lista[1]:
+         wynik.append(i)
---
+++ src/system_binerny.py
@@ -0,0 +1,1 @@
+primt("syste binarny")
