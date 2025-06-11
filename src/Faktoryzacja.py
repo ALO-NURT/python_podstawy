d12634f45b7bac6c4c61652372d0131fd20ae4c1
antonina zawierta <zawi-an@nurt.net.pl>
2025-06-11 09:00:26 +0200

    lekcja 11.06.2025

--- src/Faktoryzacja.py
+++ src/Faktoryzacja.py
@@ -3,8 +3,27 @@ from math import sqrt
 def liczba_pierwsza(n):
     if n < 2 :
         return False
-   for i in range(2, int(sqrt(n))):
+    for i in range(2, int(sqrt(n))+1):
        if n % i == 0:
            return False
-   return True
-print(liczba_pierwsza(15))
+    return True
+print(liczba_pierwsza(25))
+
+
+
+# lekcja 11.06 sito erastotenesa
+
+def sito_erastotenesa(n):
+    if n < 2:
+        return ([])
+    lista = [True] * (n + 1)
+    lista[0] = False
+    lista [1] = False
+
+    for indeks in range (2, int(n**0.5)+1):
+        if lista[indeks]:
+            for i in range (2*indeks, n+1, indeks):
+                lista[i] = False
+    return(lista)
+
+print(sito_erastotenesa(10))
