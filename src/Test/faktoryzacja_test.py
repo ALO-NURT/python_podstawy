e9458c47eead8233de364b5291f0d188a05d640c
Stanisław Kneć <Knec-St@nurt.net.l>
2025-06-13 08:15:06 +0200

    13.06.2025 again

---
+++ src/Test/faktoryzacja_test.py
@@ -0,0 +1,5 @@
+from sito_erastotenesa import sito_erastotenesa
+
+def sito_zwraca_liczby_pierwsze():
+    assert sito_erastotenesa(10) == [2,3,5,7]
+    assert sito_erastotenesa(23) == [ 2,3,5,7,11,13,14,23]
