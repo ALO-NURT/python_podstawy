ac374990044c3538cfec7a1155a363a9515478fe
jan_rychlicki <Rych-Ja@nurt.net.pl>
2025-06-13 08:03:36 +0200

    lekcja 13.06.2025 sito_erastotenesa

---
+++ src/sito_erastotenesa.py
@@ -0,0 +1,17 @@
+def sito_erastotenesa(n):
+    if n<2:
+        return[]
+    lista = [True]*(n+1)
+    lista[0] = False
+    lista[1] = False
+    wynik = []
+    for indeks in range (2, n+1):
+        if lista[indeks]:
+            wynik.append(indeks)
+            for i in range (indeks, n+1, indeks):
+                lista[i] = False
+    return wynik
+
+print("Podaj liczbę:")
+n = int(input())
+print(sito_erastotenesa(n))
