c6f162495db1ff3d30b429c98146ad1c5d2b65f1
Maria Lekszycka <leks-ma@nurt.net.pl>
2025-06-18 09:02:23 +0200

    18.06.25

---
+++ src/erastotenes.py
@@ -0,0 +1,19 @@
+def sito_erastotelesa(n):
+    lista = [True] * (n + 1)
+    lista[0] = False
+    lista[1] = False
+
+    for indeks in range(2, int(n**0.5) + 1):
+        if lista[indeks]:
+            for i in range(2 * indeks, n + 1, indeks):
+                lista[i] = False
+
+    wynik = []
+    for i in range(n + 1):
+        if lista[i]:
+            wynik.append(i)
+    return wynik
+
+print(sito_erastotelesa(10))
+
+
---
+++ src/sito_erastotenesa_test.py
@@ -0,0 +1,2 @@
+from src.sito_erastotenesa
+import sito_erastotenesa assert sito_erastotenesa(20) == [2, 3, 5, 7, 11, 13, 17, 19]
