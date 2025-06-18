d123ec0fd177f847b469f6d830ade9d4cdcf04ee
Róża Figiel <figi-ro@nurt.net.pl>
2025-06-18 08:38:35 +0200

    Sito erastotelesa

--- src/sito_erastotelesa.py
+++ src/sito_erastotelesa.py
@@ -1,15 +1,16 @@
 def sito_erastotelesa(n):
-    lista = (True) * (n + 1)
+    lista = [True]*(n + 1)
     lista[0] = False
+    lista [1] = False
     indeks = 2
     for indeks in range  (2,int(n**0.5)+1):
         if lista [indeks]:
             for i in range (2*indeks, n+1, indeks):
                 lista [i] = False
-    return (lista)
-    print (sito_arystotelesa(10))
     wynik = []
     for i in range (n+1):
-        if lista[1]
-            wynik.apperol(i)
-    return (wynik)
+        if lista [i]:
+            wynik.append(i)
+    return wynik
+
+print(sito_erastotelesa(10))
