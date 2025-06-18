2c050a705ab57057f3041781d4632e401db670da
Zuzann Szydło <szyd-zu@nurt.net.pl>
2025-06-18 08:46:33 +0200

    faktoryzacja

--- src/faktoryzacja.py
+++ src/faktoryzacja.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 
 def liczba_pierwsza (n):
-    for i in range(2 , int(sqrt(n))):
+    for i in range(2 , int(sqrt(n))+1):
         if n % i == 0:
             return False
         return True
@@ -13,3 +13,22 @@ if liczba_pierwsza(liczba):
     print(f"{liczba} jest liczbą pierwszą")
 else:
     print(f"{liczba} nie jest liczbą pierwszą")
+
+#28.05
+def sito_erastotenesa(n):
+    lista = [True]*(n + 1)
+    lista[0] = False
+    lista [1] = False
+    for indeks in range (2,int(n**0.5)+1):
+        if lista [indeks]:
+            for i in range (2*indeks , n+1 , indeks):
+                lista [i] = False
+    wynik= []
+    for i in range (n+1):
+        if lista[i]:
+            wynik.append(i)
+    return wynik
+
+print(sito_erastotenesa(400))
+
+
