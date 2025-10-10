076473a6c30e65f756264db245421fe935b794ac
Mrug-We <mrug-we@nurt.net.pl>
2025-10-10 09:06:09 +0200

    Zmiana 9 X 2025 A.D. ~Archadius

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,1 +1,11 @@
 print("system binarny")
+
+def dekoduj_system_binarny(liczba):
+    wynik=0
+    for i in range(liczba.__len__()):
+        if liczba[-i-1] == "1":
+            wynik += 2 ** (liczba.__len__()-i)
+        return wynik
+
+print(dekoduj_system_binarny("1010"))
+print(dekoduj_system_binarny("10100"))
