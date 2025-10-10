57d833d7e0404d4b06766df4b6d8d8f16134e46e
Oliwia Szymanska <szym-ol@nurt.net.pl>
2025-10-10 09:03:55 +0200

    10.10

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,1 +1,11 @@
+
 print("system binarny")
+
+def dekoduj_system_binarny(liczba):
+    wynik = 0
+    for i in (liczba.__len__()):
+        if liczba[liczba.__len__() - i - 1] == "1":
+            wynik += 1 * (2 ** (liczba.__len__() - i))
+    return wynik
+
+print(dekoduj_system_binarny("1010"))
