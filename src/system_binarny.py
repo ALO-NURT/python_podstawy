35366d327b3383083abdd3b4548428bc99a72368
Jan Rychlicki <Rych-Ja@nurt.net.pl>
2025-10-10 09:04:58 +0200

    10.10.20205 system_binarny

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -2,10 +2,10 @@ print("system_binarny")
 
 def dekoduj_system_binarny(liczba):
     wynik = 0
+    print(liczba.__len__())
     for i in range(liczba.__len__()):
         if liczba(liczba.__len__() - i - 1) == "1":
-            wynik += 1 * (2 ** liczba.__len__() - i - 1)
-    return wynik
+            wynik += 2 * (2 ** liczba.__len__() - i)
     
 
 print(dekoduj_system_binarny("1010"))
