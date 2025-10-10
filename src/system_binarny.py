0673a35fc67c58cedf52d710d55631d91ee9eb7e
Bork-He <bork-he@nurt.net.pl>
2025-10-10 09:04:42 +0200

    Merge remote-tracking branch 'origin/helena_borkowska' into helena_borkowska

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,1 +1,9 @@
 print("system binarny")
+def dekoduj_system_binarny(liczba):
+    wynik=0
+    for i in range(liczba.__len__()):
+        if liczba[-i-1] == "1":
+            wynik += 1 * (2 ** (liczba.__len__() - i))
+    return wynik
+
+print(dekoduj_system_binarny("1010"))
