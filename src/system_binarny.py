8cd04ac0dcfdd4942bc93ac0465a184efd28c236
Julia Goszczynska <goszju@nurt.net.pl>
2025-10-17 08:59:02 +0200

    praca na lekcji 17.10.2025

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,11 +1,13 @@
 print("system_binarny")
 
-def dekoduj_system_binarny(liczba):
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
     wynik = 0
-    for i in range(liczba.__len__()):
-        if liczba(liczba.__len__() - i - 1)== "1":
-            wynik += 1 * (2 ** liczba.__len__() - i - 1)
-            return wynik
+    for i in range(dlugosc_tekstu):
+         znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik += int(znak) * 2 ** i
 
+    return wynik
 
-    print(dekoduj_system_binarny("1010"))
+assert dekoduj_system_binarny("1010110") == 86
+assert dekoduj_system_binarny("1010110")
