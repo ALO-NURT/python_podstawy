613754cf2a9525763add7a885b669ad0e7be5bc9
Stanisław Kneć <knec-st@nurt.net.pl>
2025-10-17 09:01:56 +0200

    lekcja 17.20.2025

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,1 +1,12 @@
-print("system binarny")
+print("system_binarny")
+
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna. __len__()
+    wynik = 0
+    i = 0
+    for znak in liczba_binarna:
+        wynik += int(znak) * 2 ** (dlugosc_tekstu - i)
+        i += 1
+    print(wynik)
+
+dekoduj_system_binarny('10000')
