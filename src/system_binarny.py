b5646c84ad0cbe2edc030e2886a12e1d5353a4e4
Maciej Sokołowski <soko-ma@nurt.net.pl>
2025-10-17 09:01:58 +0200

    system_binarny

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,1 +1,14 @@
-print("system binarny") # ostatnie logowanie 16.10.2025 - wydaje mi się, że nadrobiłem nieobecność
+print("Liczba w systemie binarnym (a):")
+a = str(input())
+
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
+    wynik = 0
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik += int(znak) * 2 ** i
+
+    return wynik
+
+print("Liczba w systemie dziesiętnym:")
+print(dekoduj_system_binarny(a))
