a3b88b6a9ca12565795dad0018c2b72828fe4f3f
Michal Swiezy <michal.swiezy@nurt.edu.pl>
2025-10-17 08:59:26 +0200

    system binarny

---
+++ src/system_binarny.py
@@ -0,0 +1,14 @@
+print("system_binarny")
+
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
+    wynik = 0
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        print(f'{znak} * 2^{i}')
+        wynik += int(znak) * 2 ** i
+
+    return wynik
+
+assert dekoduj_system_binarny("1010110") == 86
+#assert dekoduj_system_binarny("1010")
