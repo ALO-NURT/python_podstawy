e997fcbc9a223383e559e89b151c9740fc304d94
Olac-Wi <Olac-Wi@nurt.net.pl>
2025-10-17 09:12:38 +0200

    Lekcja 17.10.2025

---
+++ src/system_binarny.py
@@ -0,0 +1,13 @@
+print("system_binarny")
+
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
+    wynik = 0
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik +=int(znak) *  2 ** i
+
+    return wynik
+
+assert dekoduj_system_binarny("1010110") == 86
+assert dekoduj_system_binarny("1010") == 10
