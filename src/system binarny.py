56cf6c8dd49ee1b0aab418460d5075422757403f
Figi-Ro <figi-ro@nurt.net.pl>
2025-10-17 09:01:09 +0200

    17.10

--- src/system binarny.py
+++ src/system binarny.py
@@ -1,11 +1,15 @@
 print("system binarny")
 
-def dekoduj_system_binarny(liczba):
-    wynik = 0
-    for i in (liczba.__len__()):
-        if liczba[liczba.__len__() - i - 1] == "1":
-            wynik += 1 * (2 ** (liczba.__len__() - i))
-    return wynik
 
-print(dekoduj_system_binarny("1010"))
-print(dekoduj_system_binarny("101"))
+print("system_binarny")
+
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
+    wynik = 0
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik += int(znak) * 2 ** i
+
+        print(wynik)
+
+dekoduj_system_binarny("1010110")
