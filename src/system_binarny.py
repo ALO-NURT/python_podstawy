fe5da91baf97c0c0635e99bfa95b03269a4c7b0c
Julia Goszczynska <goszju@nurt.net.pl>
2025-10-17 09:01:28 +0200

    praca na lekcji 17.10.2025

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -4,7 +4,7 @@ def dekoduj_system_binarny(liczba_binarna):
     dlugosc_tekstu = liczba_binarna.__len__()
     wynik = 0
     for i in range(dlugosc_tekstu):
-         znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
         wynik += int(znak) * 2 ** i
 
     return wynik
