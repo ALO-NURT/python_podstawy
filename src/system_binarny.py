de9b0d94d8ac433b073bd080cae66ecd462f0065
Duna-Ur <duna-ur@nurt.net.pl>
2025-10-17 08:59:42 +0200

    lekcja 10.10 system binarny definicja  funkcji dekoduj system binarny

---
+++ src/system_binarny.py
@@ -0,0 +1,11 @@
+print("system_binarny")
+
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
+    wynik = 0
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik += (int(znak) * 2 ** i)
+    print(wynik)
+
+dekoduj_system_binarny("1010110")
