c2b08b2123c0501ff1f904403116f9a41226278a
Mrug-We <mrug-we@nurt.net.pl>
2025-10-17 08:58:41 +0200

    Zmiana 9 X 2025 A.D. ~Archadius

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,11 +1,14 @@
 print("system binarny")
 
-def dekoduj_system_binarny(liczba):
-    wynik=0
-    for i in range(liczba.__len__()):
-        if liczba[-i-1] == "1":
-            wynik += 2 ** (liczba.__len__()-i)
-        return wynik
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu=liczba_binarna.__len__()
+    for i in range(dlugosc_tekstu):
+        wynik = 0
+        for i in range(dlugosc_tekstu):
+            znak = liczba_binarnaa[dlugosc_tekstu - i - 1]
+            print(f'{znak} * 2^{i}')
+            wynik += int(znak) * 2 ** i
 
-print(dekoduj_system_binarny("1010"))
-print(dekoduj_system_binarny("10100"))
+        return wynik
+assert dekoduj_system_binarny("1010110") == 86
+assert dekoduj_system_binarny("1010")
