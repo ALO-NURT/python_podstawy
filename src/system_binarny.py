5ae394fdd0178dad69ccb4e3a833b1e794ff29ff
Olac-Wi <Olac-Wi@nurt.net.pl>
2025-10-24 09:04:32 +0200

    lekcja 24.10.2025

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -11,3 +11,24 @@ def dekoduj_system_binarny(liczba_binarna):
 
 assert dekoduj_system_binarny("1010110") == 86
 assert dekoduj_system_binarny("1010") == 10
+
+print(dekoduj_system_binarny("1010110"))
+print(dekoduj_system_binarny("111001"))
+
+def zamień_na_system_binarny(liczba):
+    temp = int(liczba)
+    liczba_binarna = ""
+    while temp > 0:
+        reminder = temp % 2
+        temp //= 2
+        liczba_binarna = str(reminder) + liczba_binarna
+
+    return liczba_binarna
+
+print(zamień_na_system_binarny(92))
+liczba_tekstowa = 9239588
+
+binarna = zamień_na_system_binarny(liczba_tekstowa)
+dziesietna = dekoduj_system_binarny(binarna)
+
+print(f'liczba binarna: {binarna}, liczba dziesietna{dziesietna}')
