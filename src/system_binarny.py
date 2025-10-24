7ce0be5d01bf42721f8825a08bb3afc318fbac4e
Michal Swiezy <michal.swiezy@nurt.edu.pl>
2025-10-24 09:07:22 +0200

    system binarny

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -5,10 +5,28 @@ def dekoduj_system_binarny(liczba_binarna):
     wynik = 0
     for i in range(dlugosc_tekstu):
         znak = liczba_binarna[dlugosc_tekstu - i - 1]
-        print(f'{znak} * 2^{i}')
         wynik += int(znak) * 2 ** i
 
     return wynik
 
-assert dekoduj_system_binarny("1010110") == 86
-#assert dekoduj_system_binarny("1010")
+def zamien_na_system_binarny(liczba):
+    temp = int(liczba)
+    liczba_binarna = ""
+    while temp > 0:
+        remainder = temp % 2
+        temp //= 2
+        liczba_binarna = str(remainder) + liczba_binarna
+
+    return liczba_binarna
+
+#print(dekoduj_system_binarny("1010110"))
+#print(dekoduj_system_binarny("111001"))
+
+#print(zamien_na_system_binarny(92))
+
+liczba_testowa = 134567324589
+
+binarna = zamien_na_system_binarny(liczba_testowa)
+dziesietna = dekoduj_system_binarny(binarna)
+
+print(f'liczba binarna: {binarna}, liczba dziesietna: {dziesietna}')
