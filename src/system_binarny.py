6760a928851c101e01e74d64d74480e0dceaf933
JanMisTom <Rych-Ja@nurt.net.pl>
2025-10-30 22:46:59 +0100

    Zadanie domowe konwersja systemu binarnego i dziesiętnego 30.10.2025

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,6 +1,4 @@
-print("Liczba w systemie binarnym (a):")
-a = str(input())
-
+#Z systemu binarnego na system dziesiętny
 def dekoduj_system_binarny(liczba_binarna):
     dlugosc_tekstu = liczba_binarna.__len__()
     wynik = 0
@@ -10,16 +8,25 @@ def dekoduj_system_binarny(liczba_binarna):
 
     return wynik
 
-print("Liczba w systemie dziesiętnym:")
-print(dekoduj_system_binarny(a))
-
-def zaien_na_system_binarny(liczba):
+#Z systemu dziesiętnego na system binarny
+def zamień_na_system_binarny(liczba):
     temp = int(liczba)
-    liczba_binarna = **
-    while tem > 0:
+    liczba_binarna = ""
+    while temp > 0:
         reminder = temp % 2
         temp //= 2
         liczba_binarna = str(reminder) + liczba_binarna
+
     return liczba_binarna
 
-liczba_testowa = 134567324589
+#Z systemu binarnego na system dziesiętny
+print("Liczba w systemie binarnym (a):")
+a = str(input())
+print("Liczba w systemie dziesiętnym:")
+print(dekoduj_system_binarny(a))
+
+#Z systemu dziesiętnego na system binarny
+print("Liczba w systemie dziesiętnym: (b):")
+b = str(input())
+print("Liczba w systemie binarny")
+print(zamień_na_system_binarny(b))
