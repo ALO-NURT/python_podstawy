67f700c38fd064e7934e6459e853b4b62f75348a
Duna-Ur <duna-ur@nurt.net.pl>
2025-10-31 09:02:34 +0100

    lekcja 10.10 system binarny definicja  funkcji dekoduj system binarny zmiana na system binarny liczby dziesiętmej lekcja 31.10 zamiana na liczbę arną z jakiegokolwiek systemu, notatki z lekcji

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,7 +1,7 @@
 print("system_binarny")
 
 def dekoduj_system_binarny(liczba_binarna):
-    dlugosc_tekstu = liczba_binarna.__len__()
+    dlugosc_tekstu = liczba_binarna.__len__() # len to lenght czyli długość - definiujemy tę funkcję jako dlugosc_tekstu, żeby było łatwiej
     wynik = 0
     for i in range(dlugosc_tekstu):
         znak = liczba_binarna[dlugosc_tekstu - i - 1]
@@ -12,12 +12,26 @@ def dekoduj_system_binarny(liczba_binarna):
 
 def zamien_na_system_binarny(liczba):
     temp = int(liczba)
-    liczba_binarna = ""
-    while temp > 0:
-        remainder = temp % 2
-        temp //= 2
-        liczba_binarna = str(remainder) + liczba_binarna
+    liczba_binarna = "" #liczba binarna to tekst. [pusty
+    while temp > 0: #pętla
+        remainder = temp % 2 # % - modulo - reszta z dzielenia temp na 2
+        temp //= 2 # = temp = temp // 2 // to dzielenie całkowite
+        liczba_binarna = str(remainder) + liczba_binarna #string - ciąg znaków
 
     return liczba_binarna
 
-print(zamien_na_system_binarny(73))
+print(zamien_na_system_binarny(73)
+
+def zamien_na_system(podstawa)
+    temp = int(podstawa)
+    wynik = ""
+    while temp > 0
+        remainder = temp % podstawa
+        temp = temp // podstawa
+        wynik = str(remainder) + wynik
+    return wynik
+
+#nawiasy
+# () to zmienna, do której może byc funkcja
+# [] oznacza listę tutaj zeroelementowa
+# tablica.push("apple')
