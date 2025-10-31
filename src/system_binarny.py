c6076027604900015db2f8643f7c58c557119a64
Olac-Wi <Olac-Wi@nurt.net.pl>
2025-10-31 09:02:22 +0100

    lekcja 31.10.2025

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -15,11 +15,13 @@ assert dekoduj_system_binarny("1010") == 10
 print(dekoduj_system_binarny("1010110"))
 print(dekoduj_system_binarny("111001"))
 
+#temp - zmienna
+
 def zamień_na_system_binarny(liczba):
     temp = int(liczba)
     liczba_binarna = ""
-    while temp > 0:
-        reminder = temp % 2
+    while temp > 0:  #<- pętla
+        reminder = temp % 2 #<- % modulo, dla zmiennej zapisujemy reszta dzielenia przez 2
         temp //= 2
         liczba_binarna = str(reminder) + liczba_binarna
 
@@ -32,3 +34,16 @@ binarna = zamień_na_system_binarny(liczba_tekstowa)
 dziesietna = dekoduj_system_binarny(binarna)
 
 print(f'liczba binarna: {binarna}, liczba dziesietna{dziesietna}')
+
+#lekcja 31.10.2025 - zamiana z systemu binarnego na system ósemkowy
+def zamien_na_system(liczba , podstawa = 2):
+    temp = int(liczba)
+    wynik = ""
+    while temp > 0:
+        reminder = temp % podstawa
+        temp = temp // podstawa
+        wynik = str = str(reminer) + wynik
+    return wynik
+
+#() i [] - w środku nawiasów są argumenty funkcji
+#tablica = [] <- do zmiennej 'tablica' została dopisana lista o zerowej l. elementów
