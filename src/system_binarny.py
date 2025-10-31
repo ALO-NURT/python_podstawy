41cb4c7ea75298771cfe4d850d385db9aca32122
Stanisław Kneć <knec-st@nurt.net.pl>
2025-10-31 09:02:11 +0100

    31.10.2025 - lekcja

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,12 +1,29 @@
-print("system_binarny")
-
 def dekoduj_system_binarny(liczba_binarna):
-    dlugosc_tekstu = liczba_binarna. __len__()
+    dlugosc_tekstu = liczba_binarna.__len__()
     wynik = 0
-    i = 0
-    for znak in liczba_binarna:
-        wynik += int(znak) * 2 ** (dlugosc_tekstu - i)
-        i += 1
-    print(wynik)
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik += int(znak) * 2 ** i
+    
+    return wynik
 
-dekoduj_system_binarny('10000')
+def zamień_na_system_binarny(liczba):
+    temp = int(liczba)
+    liczba_binarna = ""
+    while temp > 0:
+        reminder = temp % 2
+        temp //= 2
+        liczba_binarna = str(reminder) + liczba_binarna
+    
+    return liczba_binarna
+
+print("Liczba w systemie binarnym (a):")
+a = str(input())
+print("Liczba w systemie dziesiętnym:")
+print(dekoduj_system_binarny(a))
+
+print("Liczba w systemie dziesiętnym: (b):")
+b = str(input())
+print("Liczba w systemie binarny")
+print(zamień_na_system_binarny(b))
+ 
