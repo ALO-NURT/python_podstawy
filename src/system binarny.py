b9df35111b7781b1b391eeb58412f444750aa99a
Figi-Ro <figi-ro@nurt.net.pl>
2025-10-31 09:01:00 +0100

    różne systemy liczbowe

--- src/system binarny.py
+++ src/system binarny.py
@@ -30,3 +30,13 @@ binarna = zamien_na_system_binarny(liczba_testowa)
 dziesietna = dekoduj_system_binarny(binarna)
 
 print(f'liczba_binarna: {binarna}, dziesietna: {dziesietna}')
+
+
+def zamien_na_system(liczba, podstawa = 2):
+    temp = int(liczba)
+    wynik = ""
+    while temp > 0:
+        remainder = temp%podstawa
+        temp = temp // podstawa
+        wynik = str(remainder) + wynik
+    return wynik
