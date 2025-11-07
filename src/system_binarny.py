fe006448eeb9a3768dd5833a57f7883e225f5297
stanisław drozdowski <Droz-St@nurt.net.pl>
2025-11-07 08:21:17 +0100

    lekcja system binarny

--- src/system_binarny.py
+++ src/system_binarny.py
@@ -1,4 +1,35 @@
-print("system binarny")
+def dekoduj_system_binarny(liczba_binarna):
+    dlugosc_tekstu = liczba_binarna.__len__()
+    wynik = 0
+    for i in range(dlugosc_tekstu):
+        znak = liczba_binarna[dlugosc_tekstu - i - 1]
+        wynik += int(znak) * 2 ** i
+
+    return wynik
 
 
-dekoduj-system-binarny(liczba-binarna)
+def zamień_na_system_binarny(liczba):
+    temp = int(liczba)
+    liczba_binarna = ""
+    while temp > 0:
+        reminder = temp % 2
+        temp //= 2
+        liczba_binarna = str(reminder) + liczba_binarna
+
+    return liczba_binarna
+
+
+print("Liczba w systemie binarnym (a):")
+a = str(input())
+print("Liczba w systemie dziesiętnym:")
+print(dekoduj_system_binarny(a))
+
+print("Liczba w systemie dziesiętnym: (b):")
+b = str(input())
+print("Liczba w systemie binarny")
+print(zamień_na_system_binarny(b))
+
+
+
+
+
