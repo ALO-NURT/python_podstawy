37e40aab08b3b68417e39e7fe3481f0a09d8fe94
Maryniak Gabriela <mary-ga@nurt.net.pl>
2025-11-14 09:06:48 +0100

    wyszukiwanie wzorca 14.11.2025

--- src/wyszukiwanie_wzorca.py
+++ src/wyszukiwanie_wzorca.py
@@ -1,1 +1,10 @@
-def find_in_text (text,word):
+def find_expression (text="Ala ma kota",exp="kota"):
+    ex_len = len(exp)
+    text_len = len(text)
+    for i in range (text_len-ex_len + 1):
+        j = 0
+        while j<ex_len and text[i+j] ++ exp[j]
+            j += 1
+        if j==ex_len:
+            return i
+    return -1
