4db3101de9740bd297d500a0c4ac4cbbf9347224
Olac-Wi <Olac-Wi@nurt.net.pl>
2025-11-14 09:07:01 +0100

    lekcja 14.11.2025

--- src/wyszukiwanie wzorca.py
+++ src/wyszukiwanie wzorca.py
@@ -1,1 +1,11 @@
-def find_in_text ()
+def find_in_text (text, exp):
+    ex_len = len(exp)
+    text_len = len(text)
+    for i in range(text_len - ex_len + 1):
+        j = 0
+        while j < ex_len and texgt[i + j] == text[j]
+            j = j + 1
+        if j == ex_len:
+            return i
+    return -1
+
