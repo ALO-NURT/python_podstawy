e53a5476b0e5039e97f243a85d0f0d3b71ac7c8d
Maciej Sokołowski <soko-ma@nurt.net.pl>
2025-11-14 09:04:31 +0100

    wyszukiwanie_wzorca - plik

---
+++ src/wyszukiwanie_wzorca.py
@@ -0,0 +1,15 @@
+def find_in_text(text, exp):
+    ex_len = len(exp)
+    text_len = len(text)
+
+    for i in range(text_len - ex_len + 1):
+        j = 0
+        while j < ex_len and text[i+j] == exp[j]:
+            j += 1
+    if j == ex_len:     # jeśli dopasowaliśmy cały exp
+            return i
+
+    return "nie"
+
+print(find_in_text("Ala ma kota", "kota"))
+
