388a01eef3f0aee4f63628143ab84b9c745e5d72
Stanisław Kneć <knec-st@nurt.net.pl>
2025-11-16 20:19:37 +0100

    16.11.2025 - zadanie domowe

---
+++ src/wyszukiwanie_wzorca.py
@@ -0,0 +1,15 @@
+def wyszukiwanie_wzorca(text, pattern):
+
+    matches = []
+    
+    for i in range(len(text) - len(pattern) + 1):
+        match = True
+        for j in range(len(pattern)):
+            if text[i + j] != pattern[j]:
+                match = False
+                break
+        
+        if match:
+            matches.append(i)
+    
+    return matches
