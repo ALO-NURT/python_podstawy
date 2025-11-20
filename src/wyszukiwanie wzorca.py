7701f0b851dbf3e4bf8d384880cec6048e70c27e
Wiktoria Olaczek <Olac-Wi@nurt.net.pl>
2025-11-20 23:52:11 +0100

    zadanie domowe 20.11.2025

--- src/wyszukiwanie wzorca.py
+++ src/wyszukiwanie wzorca.py
@@ -9,3 +9,16 @@ def find_in_text (text, exp):
             return i
     return -1
 
+def wyszukiwanie_wzorca(text,pattern):
+    matches = []
+    for i in range(len(text) - len(pattern) +1):
+        match = True
+        for j in range(len(pattern)):
+            if text[i + j] != pattern[j]:
+                match = False
+                break
+        if match:
+            matches.append(i)
+        return matches
+print(wyszukiwanie_wzorca("wyszukiwanie", "wzorca")
+
