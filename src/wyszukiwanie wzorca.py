bdb91a5fbe4f8022578835a0e6e376badad9c520
Oliwia Szymanska <szym-ol@nurt.net.pl>
2025-11-20 08:49:39 +0100

    zadanie domowe 21.11.2025

--- src/wyszukiwanie wzorca.py
+++ src/wyszukiwanie wzorca.py
@@ -1,4 +1,14 @@
-def find_in_text (text ,word):
-    words = text.split()
-    for word in words:
-        if word in word:
+
+def wyszukiwanie_wzorca(text,pattern):
+    matches = []
+    for i in range(len(text) - len(pattern) + 1):
+        match = True
+        for j in range(len(pattern)):
+            if text[ i + j] != pattern[j]:
+                match= False
+                break
+        if match:
+            matches.append(i)
+    return matches
+
+print(wyszukiwanie_wzorca("banana", "ana"))
