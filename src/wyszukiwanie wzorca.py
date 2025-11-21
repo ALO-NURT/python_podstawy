bc21c912117ab54ae774995188663189fade3963
Bork-He <bork-he@nurt.net.pl>
2025-11-21 09:05:49 +0100

    Merge remote-tracking branch 'origin/helena_borkowska' into helena_borkowska

--- src/wyszukiwanie wzorca.py
+++ src/wyszukiwanie wzorca.py
@@ -1,10 +1,10 @@
 
-def find_expression(text, ex):
-    ex_len = len(ex)
+def find_esxpression(text, exp):
+    ex_len = len(exp)
     text_len = len(text)
     for i in range(text_len - ex_len + 1):
         j = 0
-        while j < ex_len and text[i + j] == ex[j]:
+        while j < ex_len and text[i + j] == exp[j]:
             j = j + 1
         if j == ex_len:
             return i
@@ -14,5 +14,5 @@ with open("test_data.txt") as file:
     lines = [line.rstrip() for line in file]
 
 text = lines[0]
-pattern = lines[0]
-print(find_expression(text, pattern))
+pattern = lines[1]
+print(find_esxpression(text, pattern))
