1d9e653756b4cdc2b2d64fc093b21930f750cb64
Maryniak Gabriela <mary-ga@nurt.net.pl>
2025-11-21 08:35:26 +0100

    wyszukiwanie wzorca 14.11.2025, zadanie domowe

--- src/wyszukiwanie_wzorca.py
+++ src/wyszukiwanie_wzorca.py
@@ -1,10 +1,22 @@
-def find_expression (text="Ala ma kota",exp="kota"):
+def find_expression (text='Ala ma kota',exp='kota'):
     ex_len = len(exp)
     text_len = len(text)
     for i in range (text_len-ex_len + 1):
         j = 0
-        while j<ex_len and text[i+j] ++ exp[j]
+        while j < ex_len and text[i+j] == exp[j]:
             j += 1
         if j==ex_len:
             return i
     return -1
+
+def wyszukiwanie_wzorca(text,a):
+    matches = []
+    for i in range(len(text) - len(a) + 1):
+        match = True
+        for j in range(len(a)):
+            if text[ i + j] != a[j]:
+                match= False
+                break
+        if match:
+            matches.append(i)
+    return matches
