7d53b4de38138de48502e014105ab462fec94cb9
stanisław drozdowski <Droz-St@nurt.net.pl>
2025-11-21 08:37:10 +0100

    lekcja system binarny

---
+++ src/wyszukiwania_wzorca.py
@@ -0,0 +1,11 @@
+def find_expression (text, ex):
+    ex_len = len(ex)
+    text_len = len(text)
+    for i in range (text_len - ex_len + 1):
+        j = 0
+        while j < ex_len and text[i + j] == ex[j]:
+            j = j + 1
+        if j == ex_len:
+            return i
+    return -1
+print(find_expression("sok jabłko mientowy", "ja"))
