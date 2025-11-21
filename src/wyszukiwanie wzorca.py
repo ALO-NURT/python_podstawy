b00ca772878ae138f300dbee13e0b2f4843c8bcf
Szyd-Zu <szyd-zu@nurt.net.pl>
2025-11-21 08:32:16 +0100

    lekcja 14.10 i zadanie domowe

--- src/wyszukiwanie wzorca.py
+++ src/wyszukiwanie wzorca.py
@@ -9,3 +9,18 @@ def find_expression (text='ala ma kota',exp= 'kota'):
                 if j == ex_len:
                     return i
     return -1
+
+def wyszukiwanie_wzorca(text,bla):
+    matches = [ ]
+    for i in range(len(text) - len(bla) + 1):
+        match = True
+        for j in range(len(bla)):
+            if text[ i + j] != bla [j]:
+                match = False
+                break
+        if match:
+            matches.append(i)
+
+    return matches
+    print(wyszukiwanie_wzorca(text,bla))
+
