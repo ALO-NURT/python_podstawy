f3814945bfe1a7a32b60995f8737b64fb4c9e28b
Maciej Sokołowski <soko-ma@nurt.net.pl>
2025-11-21 09:04:53 +0100

    wyszukiwanie_wzorca - plik

---
+++ src/wzorzec.py
@@ -0,0 +1,19 @@
+def find_in_text(text, exp):
+    ex_len = len(exp)
+    text_len = len(text)
+
+    for i in range(text_len - ex_len + 1):
+        j = 0
+        while j < ex_len and text[i+j] == exp[j]:
+            j += 1
+        if j == ex_len:
+            return i
+
+    return -1
+
+with open("text_data.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+text = lines[0]
+pattern = lines[1]
+print(find_expression(text, pattern))
