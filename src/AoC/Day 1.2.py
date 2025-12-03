c78e49c796d9214f18514be3a2fd650373e12ba2
AlvdelPor <soko-ma@nurt.net.pl>
2025-12-03 18:52:17 +0100

    AoC Day 1.2

---
+++ src/AoC/Day 1.2.py
@@ -0,0 +1,58 @@
+def tekst_na_liste(tekst):
+    lista = []
+    aktualne = ""
+
+    for znak in tekst:
+        if znak == ",":
+            if aktualne != "":
+                lista.append(aktualne)
+                aktualne = ""
+        elif znak != " ":
+            aktualne += znak
+
+    if aktualne != "":
+        lista.append(aktualne)
+
+    return lista
+
+def odległosc_bazy(instrukcje):
+    kierunek = 0
+    # 0 - północ
+    # 1 - wschód
+    # 2 - południe
+    # 3 - zachód
+    x = 0
+    y = 0
+
+    odwiedzone = [(0, 0)]
+
+    for i in instrukcje:
+        obrót = i[0]
+        kroki = int(i[1:])
+
+        if obrót == "R":
+            kierunek = (kierunek + 1) % 4
+        if obrót == "L":
+            kierunek = (kierunek - 1) % 4
+
+        if kierunek == 0:
+            y += kroki
+        elif kierunek == 1:
+            x += kroki
+        elif kierunek == 2:
+            y -= kroki
+        else:
+            x -= kroki
+
+        if (x, y) in odwiedzone:
+            return abs(x) + abs(y)
+
+        odwiedzone.append((x, y))
+    return None
+
+
+print("Instrukcje:")
+tekst = str(input())
+a = tekst_na_liste(tekst)
+print("Odelgłość")
+print(odległosc_bazy(a))
