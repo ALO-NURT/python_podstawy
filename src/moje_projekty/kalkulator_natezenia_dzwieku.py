318ffbb7f0711f78547610b8653bfd344fdfb557
AlvdelPor <soko-ma@nurt.net.pl>
2025-12-07 18:27:26 +0100

    kalkulatro natezenia dzwieku

--- src/moje_projekty/kalkulator_natezenia_dzwieku.py
+++ src/moje_projekty/kalkulator_natezenia_dzwieku.py
@@ -10,15 +10,19 @@ def dzialanie(znak_dzialania, poziom_natezenia_1, poziom_natezenia_2):
     elif znak_dzialania == "-":
         wynik_posredni = natezenie_dzwieku(poziom_natezenia_1) - natezenie_dzwieku(poziom_natezenia_2)
     wynik = 10 * math.log10(wynik_posredni / (10 ** (-12)))
-    return wynik
+    return round(wynik, 5)
 
+def program():
+    print("KALKULATOR POZIOMU NATĘŻENIA DŹWIĘKU")
+    znak_dzialania = str(input("Podaj operator działania (+/-): "))
+    poziom_natezenia_1 = int(input("Podaj wartość poziomu natężenia pierwszego dźwięku: "))
+    poziom_natezenia_2 = int(input("Podaj wartość poziomu natężenia drugiego dźwięku: "))
+    print("Wynik: ", dzialanie(znak_dzialania, poziom_natezenia_1, poziom_natezenia_2))
 
-print("KALKULATOR POZIOMU NATĘŻENIA DŹWIĘKU")
-print("Podaj operator działania (+/-):")
-znak_dzialania = str(input())
-print("Podaj wartość poziomu natężenia pierwszego dźwięku:")
-poziom_natezenia_1 = int(input())
-print("Podaj wartość poziomu natężenia drugiego dźwięku:")
-poziom_natezenia_2 = int(input())
-print("Wynik:")
-print(dzialanie(znak_dzialania, poziom_natezenia_1, poziom_natezenia_2))
+while True:
+    program()
+    uruchamianie = input("Czy chcesz uruchomić program ponownie? (tak/nie):").lower()
+
+    if uruchamianie != "tak":
+        print("Zamykanie programu...")
+        break
