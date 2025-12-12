8d45e806b08ff7c9fff3e9a01463e5bbf9bc395d
Michał Świeży <michal.swiezy@gmail.com>
2025-12-12 23:54:55 +0100

    analiza czestosci

--- src/szyfr_odwrócony.py
+++ src/szyfr_odwrócony.py
@@ -1,6 +1,8 @@
 #def szyfr_odwroc(text):
  #   szyfr_odwroc(text) == text.reverse()
 
+# text.reverse() akurat dla list działa, ale nie dla stringów
+
 
 
 text = ['a','l','a','m','a','k','o','t','a']
@@ -9,6 +11,10 @@ print(text)
 
 def szyfr_odwroc(text):
     ret = ""
-    for item in list:
+    for item in list: # list nie jest zdefiniowana - powinno być text
         ret += item + ""
+    # można tekst obrócić w taki sposób, tylko trzeba doklejać kolejne litery na początek - tutaj są doklejane na koniec
 text[::-1]
+
+# zastanów się, jak dokończyć to zadanie - powyższa funkcja prawie obraca tekst, ale jest jeszcze niepoprawna (mogłaby służyć do odwracania pojedynczych słów)
+# w zadaniu chodzi o zachowanie kolejności słów, ale odwrócenie kolejności liter w każdym słowie
