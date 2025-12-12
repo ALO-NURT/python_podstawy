07bde14d7bb81b184805d53b8d36a95bbc4babe8
duna-ur <duna-ur@nurt.net.pl>
2025-12-12 08:58:32 +0100

    utworzpono plik szyfr_odwrócony.py

---
+++ src/szyfr_odwrócony.py
@@ -0,0 +1,14 @@
+#def szyfr_odwroc(text):
+ #   szyfr_odwroc(text) == text.reverse()
+
+
+
+text = ['a','l','a','m','a','k','o','t','a']
+text.reverse()
+print(text)
+
+def szyfr_odwroc(text):
+    ret = ""
+    for item in list:
+        ret += item + ""
+    text[::-1]
