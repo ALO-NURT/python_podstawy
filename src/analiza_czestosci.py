edf63bc2ffe799ca5ffadf6ed5a688fca8f0f79c
Maciej Sokołowski <soko-ma@nurt.net.pl>
2025-12-19 09:03:09 +0100

    analiza_czestosci.py

--- src/analiza_czestosci.py
+++ src/analiza_czestosci.py
@@ -1,1 +1,11 @@
-def analiza_czestosci
+def analiza_czestosci(tekst):
+    alfabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "w", "y", "z"]
+
+    for znak in alfabet:
+        licznik = 0
+        for litera in tekst:
+            if litera == znak:
+                licznik += 1
+        print(znak, licznik)
+
+print(analiza_czestosci("ala ma kota"))
