6f47dfd8f00abc31e2456f06e4f994400bd65af5
jagna biedka <bied-ja@nurt.net.pl>
2025-12-22 14:18:22 +0100

    naiwny algorytm wyszukiwania wzorca skończony

--- src/wyszukiwanie wzorca.py
+++ src/wyszukiwanie wzorca.py
@@ -1,3 +1,20 @@
 # nalezy stworzyć funkcję find_in_text(text,
 
-def find_in_text(text,word):
+def find_in_text(wzorzec, tekst):
+    n = len(wzorzec);
+    k = len(tekst)
+    for i in range(k - n + 1):
+        for j in range(n):
+            if tekst[i + j] != wzorzec[j]:
+                break
+            elif j == n - 1:
+                return True
+    return False
+
+
+tekst = input("Podaj tekst: ")
+wzorzec = input("Podaj wzorzec: ")
+if find_in_text(wzorzec, tekst):
+    print(f"wzorzec \"{wzorzec}\" występuje w tekście \"{tekst}\"")
+else:
+    print(f"wzorzec \"{wzorzec}\" nie występuje w tekście \"{tekst}\"")
