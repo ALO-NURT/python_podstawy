ac3394c1a6f3dc5680d0c91314ea45a69e8f9c61
jagna biedka <bied-ja@nurt.net.pl>
2025-12-22 13:54:08 +0100

    w pełni funkcjonalna doskonała funkcja szyfr odwrócony

--- src/szyfr_odwrócony.py
+++ src/szyfr_odwrócony.py
@@ -3,18 +3,9 @@
 
 # text.reverse() akurat dla list działa, ale nie dla stringów
 
-
-
-text = ['a','l','a','m','a','k','o','t','a']
-text.reverse()
-print(text)
-
+text = "Ala ma kota"
 def szyfr_odwroc(text):
-    ret = ""
-    for item in list: # list nie jest zdefiniowana - powinno być text
-        ret += item + ""
-    # można tekst obrócić w taki sposób, tylko trzeba doklejać kolejne litery na początek - tutaj są doklejane na koniec
-text[::-1]
-
-# zastanów się, jak dokończyć to zadanie - powyższa funkcja prawie obraca tekst, ale jest jeszcze niepoprawna (mogłaby służyć do odwracania pojedynczych słów)
-# w zadaniu chodzi o zachowanie kolejności słów, ale odwrócenie kolejności liter w każdym słowie
+    list = text.split(" ")
+    for item in list:
+        print(item[::-1])
+print(szyfr_odwroc(text))
