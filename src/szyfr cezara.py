9bc7640d4fa7a1a1f23f7ed93e108027e150dd92
roza figiel <rozafigiel@op.pl>
2026-01-15 20:24:29 +0100

    zadanie dobra wersja

--- src/szyfr cezara.py
+++ src/szyfr cezara.py
@@ -1,23 +1,17 @@
-alfabet = ["a", "ą", "b", "c", "ć", "d", "e", "ę", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o", "ó", "p", "r",
-           "s", "ś", "t", "u", "w", "y", "z", "ź", "ż"]
+alfabet = ["a", "ą", "b", "c", "ć", "d", "e", "ę", "f", "g",
+           "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o",
+           "ó", "p", "r", "s", "ś", "t", "u", "w", "y", "z",
+           "ź", "ż"]
 
 def szyfr_cezara(text, shift):
-    szyfr = []
-    for znak in text:
-        if znak.lower() in alfabet:
-            litera = alfabet.index(znak.lower())
-            nowa_litera = alfabet[(litera + shift) % len(alfabet)]
-            if znak != znak.lower():
-                szyfr.append(nowa_litera.upper())
-            else:
-                szyfr.append(nowa_litera)
+    szyfr = ""
+    for znak in text.lower():
+        if znak in alfabet:
+            index = (alfabet.index(znak) + shift) % len(alfabet)
+            szyfr = szyfr + alfabet[index]
         else:
-            szyfr.append(znak)
-    return "".join(szyfr)
+            szyfr = szyfr + znak
+    return szyfr
 
- print("Niezaszyfrowany tekst:")
-    tekst = str(input())
-    print("Przesunięcie:")
-    przesuniecie = int(input())
-    print("Zaszyfrowany tekst:")
-    print(szyfr_cezara(tekst, przesuniecie))
+tekst = "Ala ma perę kotów"
+print(szyfr_cezara(tekst, 3))
