32e08dba7d6accc6e6b368d9f17974bc047deb23
Olac-Wi <Olac-Wi@nurt.net.pl>
2026-01-16 09:04:03 +0100

    lekcja 16.01.2026

---
+++ src/proste szyfrowanie.py
@@ -0,0 +1,11 @@
+def szyfr_odwroc(text):
+    words = text.split()
+    zaszyfrowane = []
+
+    for w in words:
+        odwrocone= w[::-1]
+        zaszyfrowane.append(odwrocone)
+    return''.join(zaszyfrowane)
+
+tekst = "Ala ma kota"
+print(szyfr_odwroc(tekst))
