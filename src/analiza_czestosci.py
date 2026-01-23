fa91282a276d50715f1b32ea4ce4a01ac19ea797
Jan Rychlicki <Rych-Ja@nurt.net.pl>
2026-01-23 09:04:48 +0100

    23.01.2026 analiza_czestosci

--- src/analiza_czestosci.py
+++ src/analiza_czestosci.py
@@ -1,1 +1,20 @@
-def analiza_czestosci():
+text = ""
+
+with open("test_data.txt", encoding="utf-8") as file:
+    text = file.readline()
+
+#print(text)
+
+litery = {}
+ilosc = 0
+for znak in text:
+    lower = znak.lower()
+    if lower != " ":
+        ilosc += 1
+        if lower in litery:
+            litery[lower] += 1
+        else:
+            litery[lower] = 1
+
+for litera in litery.keys():
+    print(f'"{litera}",{litery[litera] / ilosc}')
