5b87bd61306ee522498addb181ef9f079774a194
Stanisław Kneć <knec-st@nurt.net.pl>
2026-01-23 09:04:55 +0100

    23.01.2026 - lekcja z analizą

---
+++ src/analiza_czestosci.py
@@ -0,0 +1,21 @@
+text = ""
+
+with open("test_data.txt", encoding="utf_8") as file:
+    text = file.readline()
+
+
+#print(text)
+
+litery = {}
+ilosc = 0
+for znak in text:
+    lower = znak.lower()
+    if lower != "":
+        ilosc += 1
+        if lower in litery:
+            litery[lower] += 1
+        else:
+            litery[lower] = 1
+
+for litera in litery.keys():
+    print(f'"{litera}",{litery[litera] / ilosc}')
