37844946dbf9a35eaf2e3f97ebda73f6e33c7a87
Maryniak Gabriela <mary-ga@nurt.net.pl>
2026-01-30 09:04:28 +0100

    Merge remote-tracking branch 'origin/gabriela_maryniak' into gabriela_maryniak 30.01.2026

--- src/analiza_czestosci.py
+++ src/analiza_czestosci.py
@@ -3,17 +3,17 @@
 with open("test_data.txt", encoding="utf-8") as file:
     text = file.readline()
 #print(text)
-
+polskie_litery = {'a','ą','b','c','ć','d','e','ę','f','g','h','i','j','k','l','ł','m','n','ń','o','ó','p','q','r','s','ś','t','u','v','w','x','y','z','ź','ż'}
 litery = {}
 ilosc = 0
 for znak in text:
     lower = znak.lower()
-    if lower != " ":
+    if lower in polskie_litery:
         ilosc += 1
         if lower in litery:
             litery[lower] += 1
-    else:
-        litery[lower] = 1
+        else:
+            litery[lower] = 1
 
 for litera in litery.keys():
     print(f'"{litera}", {litery[litera] / ilosc}')
