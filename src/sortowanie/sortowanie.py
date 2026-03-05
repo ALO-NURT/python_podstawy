548c096bec32f93fc2921327f1a7b8c5d934365f
AlvdelPor <soko-ma@nurt.net.pl>
2026-03-05 17:51:37 +0100

    sortowanie

---
+++ src/sortowanie/sortowanie.py
@@ -0,0 +1,27 @@
+def utworz_liste_wejsciowa(wejscie):
+    liczby = wejscie.split(", ")
+    lista_wejsciowa = []
+    for liczba in liczby:
+        lista_wejsciowa.append(int(liczba))
+    return lista_wejsciowa
+
+def sortowanie(lista_wejsciowa):
+    lista_posortowana = []
+    for liczba in lista_wejsciowa:
+        lewo = 0
+        prawo = len(lista_posortowana)
+        while lewo != prawo:
+            srodek = (lewo + prawo) // 2
+            if lista_posortowana[srodek] < liczba:
+                lewo = srodek + 1
+            else:
+                prawo = srodek
+        lista_posortowana.insert(lewo, liczba)
+    return lista_posortowana
+
+
+if __name__ == "__main__":
+    print("Liczby do posortowania (odzdzielone przecinkiem):")
+    dane = str(input())
+    print("Posortowane liczby:")
+    print(lsortowanie(utworz_liste_wejsciowa(dane)))
---
+++ src/sortowanie/test_sortowanie.py
@@ -0,0 +1,12 @@
+import random
+from sortowanie import sortowanie
+
+losowe_liczby = []
+for i in range(1000):
+    nowa_liczba = random.randint(0, 100000)
+    losowe_liczby.append(nowa_liczba)
+
+liczby_posortowane = sortowanie(losowe_liczby)
+liczby_posortowane_automatycznie = sorted(losowe_liczby)
+print(liczby_posortowane)
+print (liczby_posortowane == liczby_posortowane_automatycznie)
