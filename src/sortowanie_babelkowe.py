4a93fbb3fd8cd0a2656764ab594c3fe02b430b5e
Olac-Wi <Olac-Wi@nurt.net.pl>
2026-03-06 09:02:36 +0100

    lekcja 6/03/2026

--- src/sortowanie_babelkowe.py
+++ src/sortowanie_babelkowe.py
@@ -1,1 +1,15 @@
-.
+def bubble_sort(table):
+    sorted = False
+    length = len(table)
+    while not sorted:
+        sorted = True
+        for i in range(length -1):
+            if table[i] > table[i+1]:
+                temp = table[i]
+                table[i] = table[i +1]
+                table[i+1] = temp
+                sorted = False
+    return table
+
+
+print(bubble_sort([4,6,2,1,5,8,3]))
