4d42dbe17c0f35771e6eacdc2c7ca9a2238b4070
Julia Goszczynska <goszju@nurt.net.pl>
2026-03-06 09:02:12 +0100

    06.03.2026 sortowanie babelkowe

---
+++ src/sortowanie_babelkowe.py
@@ -0,0 +1,16 @@
+def bubble_sort(table):
+    sorted = False
+    lenght = len(table)
+    while not sorted:
+        sorted = True
+        for i in range(lenght - 1):
+            if table[i] > table[i+1]:
+                temp = table[i]
+                table[i] = table[i+1]
+                table[i+1] = temp
+                sorted = False
+    return table
+
+table = [4, 6, 2, 1, 5, 8, 3]
+print(bubble_sort(table))
+print(table)
