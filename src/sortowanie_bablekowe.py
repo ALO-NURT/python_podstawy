8e1b06a3fb265b5acb7117147129c18c4bf336fe
Figi-Ro <figi-ro@nurt.net.pl>
2026-03-06 09:02:39 +0100

    06.03

---
+++ src/sortowanie_bablekowe.py
@@ -0,0 +1,14 @@
+def bubble_sort (table):
+    sorted = False
+    while not sorted:
+        for i in range(len(table) - 1):
+            if table[i] > table[i+1]:
+                temp = table[i]
+                table[i] = table[i+1]
+                table[i+1] = temp
+                sorted = False
+    return table
+
+table = [4, 6, 2, 1, 5, 8, 3]
+print(bubble_sort(table))
+print(table)
