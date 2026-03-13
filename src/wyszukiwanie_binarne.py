4aee52e4697b33a2f9d1c25d93259447af3fe540
Olac-Wi <Olac-Wi@nurt.net.pl>
2026-03-13 08:38:12 +0100

    początek pracy na lekcji 13/03/2026

---
+++ src/wyszukiwanie_binarne.py
@@ -0,0 +1,7 @@
+test = "abcdefghijk"
+lista_liczb = [1,2,3,4,5,6,7,8,9,10]
+lista_imion = ["Gabrysia", "Hela", "Jagna", "Jan", "Julia", "Maciej", "Oliwia"]
+
+print(len(test))
+print(len(lista_liczb))
+print(len(lista_imion))
