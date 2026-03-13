36ab6d6de875e6a1e8530c441ae00d560deecbbb
Maryniak Gabriela <mary-ga@nurt.net.pl>
2026-03-13 09:01:45 +0100

    początek lekcji 13.03.2026

--- wyszukiwanie_binarne.py
+++ wyszukiwanie_binarne.py
@@ -5,3 +5,17 @@ lista_imion = ["Gabrysia", "Hela", "Jan","Julia","Jagna","Maciej","Oliwia"]
 print(len(tekst))
 print(len(lista_liczb))
 print(len(lista_imion))
+
+def wyszukiwanie_binarne (lista , element):
+    początek = 0
+    koniec = len(lista)
+    while koniec > początek:
+        mid = (koniec + początek) //2
+        if lista [mid] == element:
+            return mid
+        elif lista [mid] > element:
+            koniec = mid
+        else:
+            początek = mid
+    return -1
+
