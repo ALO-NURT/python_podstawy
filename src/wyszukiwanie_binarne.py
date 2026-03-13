39a79b933866dab7d8e77f8b96cd7daa092708d4
Oliwia Szymanska <szym-ol@nurt.net.pl>
2026-03-13 08:38:43 +0100

    praca na lekcji 13.03.2026

---
+++ src/wyszukiwanie_binarne.py
@@ -0,0 +1,6 @@
+tekst ="abcderghijk"
+lista_liczb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_imion = ["Gabrysia", "Hela", "Jagna", "Jan", "Julia", "Maciej", "Oliwia"]
+print(len(tekst))
+print(len(lista_liczb))
+print(len(lista_imion))
