1a68d6e1c7f41d26e7416e392439c5bbd48efa5c
Oliwia Szymanska <szym-ol@nurt.net.pl>
2026-03-13 09:02:30 +0100

    praca na lekcji 13.03.2026

--- src/wyszukiwanie_binarne.py
+++ src/wyszukiwanie_binarne.py
@@ -1,6 +1,18 @@
 tekst ="abcderghijk"
 lista_liczb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 lista_imion = ["Gabrysia", "Hela", "Jagna", "Jan", "Julia", "Maciej", "Oliwia"]
-print(len(tekst))
-print(len(lista_liczb))
-print(len(lista_imion))
+
+def wyszukiwanie_binarne(lista, element):
+    poczatek = 0
+    koniec = len(lista)
+    while koniec > poczatek:
+        mid = (koniec + poczatek)//2
+        if lista[mid] == element:
+            return mid
+        elif lista[mid] > element:
+            koniec = mid
+        else:
+            poczatek = mid
+    return -1
+
+print(wyszukiwanie_binarne(lista_imion, "Oliwia"))
