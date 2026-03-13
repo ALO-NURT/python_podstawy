4cd2033e8d0c000168db794c6e749c5fd38491f8
stanisław drozdowski <Droz-St@nurt.net.pl>
2026-03-13 08:22:47 +0100

    odwracanie_szyfru

---
+++ src/sortowanie_bombelkowe.py
@@ -0,0 +1,15 @@
+def bubble_sort(table):
+    sorted=false
+    while not sorted
+        for 1 in range(lenght -1):
+            sorted = true
+            if table(i) > table {1+1}
+            temp = table[i]
+            table[i] = table[i + 1]
+            table[i + 1] = temp
+            sorted = False
+        return table
+
+        table = [6, 8, 7, 2, 1, 0, 4, 5, 3, 9]
+        print(table)
+        print(bubble_sort(table))
