32e0ed23251b0eaf1aeb745f6dabf01aa1dfa7dd
Wojciech Maniak <mani-wo@nurt.net.pl>
2026-03-13 08:59:22 +0100

    13.03.2025

--- src/test/wyszukiwanie_binarne.py
+++ src/test/wyszukiwanie_binarne.py
@@ -5,3 +5,16 @@ lista_imion = ["gabrysia", "hela", "jagna", "jan", "maciej", "julia", "oliwia"]
 print(len(tekst))
 print(len(lista_liczb))
 print(len(lista_imion))
+
+def wyszukiwanie_binarne(lista, element):
+    poczatek = 0
+    koniec = len(lista)
+    while koniec > poczatek:
+        mid = (koniec + poczatek) // 2
+        if lista[mid] == element:
+            return mid
+        elif lista[mid] > element:
+            koniec = mid
+        else:
+            poczatek = mid
+        return -1
