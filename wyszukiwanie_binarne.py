d3406b6d2e88cd32aac65827ccf23c35ebfa2071
Maryniak Gabriela <mary-ga@nurt.net.pl>
2026-03-13 08:38:56 +0100

    początek lekcji 13.03.2026

---
+++ wyszukiwanie_binarne.py
@@ -0,0 +1,7 @@
+tekst = "abcdefghijk"
+lista_liczb = [1,2,3,4,5,6,7,8,9,10]
+lista_imion = ["Gabrysia", "Hela", "Jan","Julia","Jagna","Maciej","Oliwia"]
+
+print(len(tekst))
+print(len(lista_liczb))
+print(len(lista_imion))
