e56ea7ce81cd5953ac94cec7f701721acaac0344
Wojciech Maniak <mani-wo@nurt.net.pl>
2026-03-13 08:38:05 +0100

    13.03.2025

---
+++ src/test/wyszukiwanie_binarne.py
@@ -0,0 +1,7 @@
+tekst = "abcdefghij"
+lista_liczb = ["a, b, c, d, e, f, g, h, i, j"]
+lista_imion = ["gabrysia, hela, jagna, jan, maciej, julia, oliwia"]
+
+print(len(tekst))
+print(len(lista_liczb))
+print(len(lista_imion))
