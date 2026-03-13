b86a4bf9c371e40aa5ee85c31708ebfa7c4682fd
Mrug-We <mrug-we@nurt.net.pl>
2026-03-13 08:37:57 +0100

    Zmiana 9 X 2025 A.D. ~Archadius

---
+++ src/wyszukiwanie_binarne.py
@@ -0,0 +1,7 @@
+   tekst = "abcdefghijk"
+   lista_liczb=[1,2,3,4,5,6,7,8,9,10]
+   lista_słów = ["Gabi", "Hela", "Jagna", "Jan", "Julka", "Maciej"]
+
+   print(len(tekst))
+   print(;en(lista_liczb))
+   print(len(lista_słów))
