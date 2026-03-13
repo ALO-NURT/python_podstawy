48fcbb663ce576bfccf72dfa4d0f124fc8d07182
Stanisław Kneć <knec-st@nurt.net.pl>
2026-03-13 08:23:09 +0100

    13.03.2026 - nadrabianie zaległości z lekcji

---
+++ src/sortowanie_bąbelkowe.py
@@ -0,0 +1,20 @@
+from sortowanie import utworz_liste_wejsciowa
+
+def sortowanie_babelkowe(lista):
+    n = len(lista)
+    sorted = False
+    while not sorted:
+        sorted = True
+        for j in range(n - 1):
+            if lista[j] > lista[j + 1]:
+                pamiec_podreczna = lista[j]
+                lista[j] = lista[j + 1]
+                lista[j + 1] = pamiec_podreczna
+                sorted = False
+    return lista
+
+if __name__ == "__main__":
+    print("Liczby do posortowania (odzdzielone przecinkiem):")
+    dane = str(input())
+    print("Posortowane liczby:")
+    print(sortowanie_babelkowe(utworz_liste_wejsciowa(dane)))
