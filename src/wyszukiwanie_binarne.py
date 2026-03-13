0444fff7e62d68ed9871979c9ef88a9f33c0814c
Bork-He <bork-he@nurt.net.pl>
2026-03-13 08:38:30 +0100

    początek pracy na lekcji 13.03

---
+++ src/wyszukiwanie_binarne.py
@@ -0,0 +1,10 @@
+tekst = "abcdefghijk"
+lista_liczb = [1,2,3,4,5,6,7,8,9,10]
+lista_slow = ["Gabi","Hela","Jagna","Jan","Julka","Maciej"]
+
+print(len(tekst))
+print(len(lista_liczb))
+print(len(lista_slow))
+
+
+
