1330e38a7f50eb47aea5a08f0b476a2392af24cd
Figi-Ro <figi-ro@nurt.net.pl>
2026-03-13 08:38:16 +0100

    pocz. pracy na lekcji

---
+++ src/wyszukiwanie_binarne.py
@@ -0,0 +1,7 @@
+tekst = "abcdefghijk"
+lista_liczb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_slow = ["Gabrysia", "Hela", "Jan", "Julia", "Maciej", "Oliwia"]
+
+print(len(tekst))
+print(len(lista_liczb))
+print(len(lista_slow))
