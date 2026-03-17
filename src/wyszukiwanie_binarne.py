86e852c74cc40116bfd21f346014342b130cc852
Stachu Kneć <Knec-St@nurt.net.pl>
2026-03-17 22:04:10 +0100

    17.03.2026 - poprawa kodu z lekcji

--- src/wyszukiwanie_binarne.py
+++ src/wyszukiwanie_binarne.py
@@ -1,16 +1,17 @@
 tekst = "abcdefghijk"
 lista_liczb = [1,2,3,4,5,6,7,8,9,10]
 lista_imion = ["Gabrysia", "Hela", "Jan", "Julia", "Jagna", "Maciej", "Oliwia", "Stasiu"]
+
 def wyszukiwanie_binarne(lista,element):
     poczatek = 0
-    koniec = len(lista)
-    while koniec>poczatek: # dlaczego ta pętla się nie skończy?
+    koniec = len(lista) -1
+    while koniec > poczatek: # dlaczego ta pętla się nie skończy?
         mid = (koniec + poczatek) //2
     if lista[mid] == element:
         return mid
     elif lista[mid] > element:
         koniec = mid
     else: 
-        poczatek = mid
+        poczatek = mid + 1
     return -1
 
