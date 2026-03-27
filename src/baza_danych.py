aa14e01d5191d4a2dbf4bcc3b733cd573909c2c8
Jan Rychlicki <Rych-Ja@nurt.net.pl>
2026-03-27 08:59:34 +0100

    27.03.2026 baza_danych

---
+++ src/baza_danych.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host="alo-nurt.pl",
+    user="alo-uczen",
+    password="ALO.mysql9",
+    database="alo_test_db"
+)
+
+cursor = database.cursor()
+cursor.execute("select pesel from bank_client")
+records = cursor.fetchall()
+
+for record in records:
+    print(record)
