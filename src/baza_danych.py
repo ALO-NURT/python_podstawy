97d31a41a5e58342c07014687cb80456915b1cc8
Julia Goszczynska <goszju@nurt.net.pl>
2026-03-27 09:01:08 +0100

    27.03.2026 oraz nadrobienie poprzedniej lekcji

---
+++ src/baza_danych.py
@@ -0,0 +1,13 @@
+import mysql.connector
+database = mysql.connector.connect(
+    host="alo-nurt.pl",
+    user="alo-uczen",
+    password="ALO.mysql9",
+    database="alo_test_db"
+)
+cursor = database.cursor()
+cursor.execute('select pesel, first_name, last_name  from bank_client')
+records = cursor.fetchall()
+
+for record in records:
+    print(record)
---
+++ src/wyszukiwanie_binarne.py
@@ -0,0 +1,20 @@
+test = "abcdefghijk"
+lista_liczb = [1,2,3,4,5,6,7,8,9,10]
+lista_imion = ["Gabrysia", "Hela", "Jagna", "Jan", "Julia", "Maciej", "Oliwia"]
+
+print(len(test))
+print(len(lista_liczb))
+print(len(lista_imion))
+
+def wyszukianie_binarne(lista, element):
+    poczatek = 0
+    koniec = len(lista)
+    while koniec > poczatek:
+        mid = ( koniec + poczatek)//2
+        if lista[mid] == element:
+            return mid
+        elif lista[mid] > element:
+            koniec = mid
+        else:
+            poczatek = mid
+    return -1
