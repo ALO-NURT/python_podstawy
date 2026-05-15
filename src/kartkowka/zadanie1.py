42292565beacfee354f71d6d414136884779637e
Mrug-We <mrug-we@nurt.net.pl>
2026-05-15 08:59:18 +0200

    Zmiana 9 X 2025 A.D. ~Archadius

---
+++ src/kartkowka/zadanie1.py
@@ -0,0 +1,25 @@
+lista_liczb=[1,2,3,4,5,6]
+def zadanie1 (lista_liczb):
+    for liczba in lista_liczb:
+        if liczba < 0:
+            print("Liczba liczb nieparzystych:", liczba)
+        if liczba % 2 == 1:
+            print("Najmniejsza liczba nieparzysta:", liczba)
+        if liczba == lista_liczb[0]+lista_liczb[6]:
+            print(liczba)
+
+
+
+
+
+
+zadanie1([1,2,3,4,5])
+
+
+
+
+
+
+
+
+
