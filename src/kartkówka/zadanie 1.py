b5734a53bd2f59fffd111b92195d060abd0483ef
Bork-He <bork-he@nurt.net.pl>
2026-05-15 09:04:01 +0200

    początek pracy na lekcji 13.03

---
+++ src/kartkówka/zadanie 1.py
@@ -0,0 +1,17 @@
+def zadanie1 (lista_liczb):
+    for liczba in lista_liczb:
+        print(liczba)
+    lista_liczb = ([1,0,-2,5,7,8])
+    for liczba in lista_liczb
+        if:
+            liczba < 0 = x
+            print(ilosc(x))
+        if:
+            liczba % 2 == 1
+                if liczba min(lista_liczb)
+                print(liczba)
+
+
+
+
+        
