f766a5809acdac54e96d4f8b81465357490e0ac6
Stachu Kneć <Knec-St@nurt.net.pl>
2026-05-21 22:18:45 +0200

    21.05.2026 - próba wykonania drugiego zadania

--- src/kartkowka/zadanie2.py
+++ src/kartkowka/zadanie2.py
@@ -1,3 +1,24 @@
-def tekst(napis):
-    napis = "Python jest Super"
-    return tekst
+def zadanie2(napis):
+    male_litery = 0
+    samogloski = ""
+    lista_samoglosek = "aeiouyąóęAEIOUYĄĘÓ"
+
+
+for znak in napis:
+    if znak >= "a" and znak <= "z":
+        male_litery = male_litery + 1
+    if znak in lista_samoglosek:
+        samogloski = samogloski + znak
+
+if napis[0] == "#" :
+    hash_znak = "TAK"
+
+else:
+    hash_znak = "NIE"
+
+print("male_litery:", male_litery)
+print("samogloski:", samogloski)
+print("hash?:", hash_znak)
+
+napis = input("Podaj napis:")
+zadanie2(napis)
