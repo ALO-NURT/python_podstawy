53cd06e4e901b64d01aa0998449e08f363974378
Stachu Kneć <Knec-St@nurt.net.pl>
2026-05-21 22:08:36 +0200

    21.05.2026 - próba poprawy zadania

--- src/kartkowka/zadanie1.py
+++ src/kartkowka/zadanie1.py
@@ -1,11 +1,32 @@
 def zadanie1(lista_liczb):
-    lista_liczb = ([1,0,5,-2,7,8])
-    return zadanie1
+    lista_liczb  = [1,0,5,-2,7,8]
+    ujemne = 0
+    najmniejsza_nieparzysta = None
+    najmniejsza = lista_liczb[0]
+    największa = lista_liczb[0]
 
-for liczba in zadanie1:
-    if liczba /2:
-        print(1) 
-    elif liczba <= 0 or liczba %2:
-        print("Brak liczba parzystych")
-    
-    print( liczba, end="")
+for liczba in lista_liczb:
+    if liczba < 0:
+        ujemne += 1
+
+    if liczba < najmniejsza:
+        najmniejsza = liczba
+
+    if liczba > największa:
+    największa = liczba
+     
+    if liczba % 2 != 0:
+         if najmniejsza_nieparzysta is None:
+                najmniejsza_nieparzysta = liczba
+
+            elif liczba < najmniejsza_nieparzysta:
+                najmniejsza_nieparzysta = liczba
+print("Liczby ujemne:", ujemne)
+
+if najmniejsza_nieparzysta is None:
+    print( "brak liczb nieparzystych")
+else:
+    print("Najmniejsza liczba nieparzysta:", najmniejsza_nieparzysta)
+
+print("Suma:", największa + najmniejsza)
+
