e8d91b0f1bf75ab7061129d17bc0dc3e3e4e54ff
AlvdelPor <soko-ma@nurt.net.pl>
2026-05-21 20:38:56 +0200

    kartkówka - zrobienie zadania 2 w domu

---
+++ src/kartkówka/zadanie_2.py
@@ -0,0 +1,24 @@
+def tekst_i_litery(tekst):
+    wynik = []
+    liczba_malych_liter = 0
+    samogloski = ["a", "ą", "e", "ę", "i", "o", "u", "y"]
+    samogloski_w_tekscie = []
+    tag = "Nie"
+
+    for litera in tekst:
+        if litera.islower():
+            liczba_malych_liter += 1
+        if litera in samogloski:
+            samogloski_w_tekscie.append(litera)
+    if tekst[0] == "#":
+        tag = "Tak"
+
+    wynik.extend([liczba_malych_liter, samogloski_w_tekscie, tag])
+    return wynik
+
+
+if __name__ == "__main__":
+    wejscie = "#Lorem ipsum dolor sit amet."
+    print("Małe litery:", tekst_i_litery(wejscie)[0])
+    print("Samogloski: ", *tekst_i_litery(wejscie)[1], sep="")
+    print("Hash:", tekst_i_litery(wejscie)[2])
