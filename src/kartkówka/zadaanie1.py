4441e2eb73c60776e779bbf4adb49b8bdadfc6c2
Wojciech Maniak <mani-wo@nurt.net.pl>
2026-05-22 08:43:03 +0200

    27.03.2025

--- src/kartkówka/zadaanie1.py
+++ src/kartkówka/zadaanie1.py
@@ -1,5 +1,7 @@
 def zadanie1(lista_liczb):
     dodatnie = 0
+    ujemne = 0
+    najmniejsze_nieparzyste = None
     najwieksza_parzysta = None
     najmniejsza = lista_liczb[0]
     najwieksza = lista_liczb[0]
@@ -8,15 +10,22 @@ def zadanie1(lista_liczb):
             najmniejsza = liczba
         if liczba > najwieksza:
             najwieksza = liczba
+        if liczba < 0:
+            ujemne += 1
+        if liczba % 2 == 1:
+            if najmniejsze_nieparzyste is None or liczba < najmniejsze_nieparzyste:
+                najmniejsze_nieparzyste = liczba
         if liczba > 0:
             dodatnie += 1
+        if liczba % 2 == 0:
             if najwieksza_parzysta is None or liczba > najwieksza_parzysta:
                 najwieksza_parzysta = liczba
     roznica = najwieksza - najmniejsza
 
-    print(f"Liczba parzystych: {dodatnie}")
+    print(f"Dodatnich: {dodatnie}")
     print(f"Największa parzysta: {najwieksza_parzysta}")
-    print(f"Różnica między największą a najmniejszą: {roznica}")
-
-lista = [2, 7, -3, 10, 5, 8, 1, -6]
+    print(f"Różnica: {roznica}")
+    print(f"Ujemnych: {ujemne}")
+    print(f"Najmniejsza nieparzysta: {najmniejsze_nieparzyste}")
+lista = [-8, 7, 3, -3, 2, 16, 9, -10, 5, 7, 17]
 zadanie1(lista)
