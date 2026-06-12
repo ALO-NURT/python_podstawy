f753e4784c294a28d2800a072fce19b3f0a3d8ad
Mrug-We <mrug-we@nurt.net.pl>
2026-06-12 08:30:17 +0200

    Zmiana 9 X 2025 A.D. ~Archadius

--- src/kartkowka/zadanie 2.py
+++ src/kartkowka/zadanie 2.py
@@ -1,1 +1,18 @@
+def tekst_i_litery(text) -> None:
+    ilosc_cyfr = 0
+    wielkie_litery = " "
 
+    for znak in text:
+        if znak.isdigit():
+            ilosc_cyfr += 1
+        elif znak.isupper():
+            wielkie_litery += znak + " "
+
+    wykrzyknik = text.endswith("!")
+
+    print(f"Ilość cyfr: {ilosc_cyfr}")
+    print(f"Wielkie litery: {wielkie_litery}")
+    print(f"Wykrzyknik: {wykrzyknik}")
+
+
+tekst_i_litery("Python3 Is Super!")
--- src/kartkowka/zadanie1.py
+++ src/kartkowka/zadanie1.py
@@ -1,11 +1,11 @@
-lista_liczb=[1,2,3,4,5,6]
+lista_liczb=[1,2,3,4,5]
 def zadanie1 (lista_liczb):
     for liczba in lista_liczb:
         if liczba < 0:
             print("Liczba liczb nieparzystych:", liczba)
         if liczba % 2 == 1:
-            print("Najmniejsza liczba nieparzysta:", liczba)
-        if liczba == lista_liczb[0]+lista_liczb[6]:
+            print(" liczba nieparzysta:", liczba)
+        if liczba == lista_liczb[0]+lista_liczb[5]:
             print(liczba)
 
 
