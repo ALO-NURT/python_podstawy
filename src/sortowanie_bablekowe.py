1d766506556f10efbefe738a2434000ad9efc91f
roza figiel <rozafigiel@op.pl>
2026-06-15 17:06:17 +0200

    znowu coś sie nie scommitowało

--- src/sortowanie_bablekowe.py
+++ src/sortowanie_bablekowe.py
@@ -1,14 +1,16 @@
-def bubble_sort (table):
+def bubble_sort(table):
     sorted = False
-    while not sorted: # czy ta pętla się skończy? I dlaczego nie?
+    while not sorted:
+        sorted = True  # czy ta pętla się skończy? I dlaczego nie?
         for i in range(len(table) - 1):
-            if table[i] > table[i+1]:
+            if table[i] > table[i + 1]:
                 temp = table[i]
-                table[i] = table[i+1]
-                table[i+1] = temp
+                table[i] = table[i + 1]
+                table[i + 1] = temp
                 sorted = False
     return table
 
+
 table = [4, 6, 2, 1, 5, 8, 3]
-print(bubble_sort(table))
 print(table)
+print(bubble_sort(table))
